eaa5cec7a5287527847f7aeefd29dd25af2f0eff
209450 <209450@edu.p.lodz.pl>
2019-11-13 23:31:47 +0100

    main container class

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import {Button, Container} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainNavBar from './components/MainNavBar'
+import MainContainer from './components/MainContainer'
 
 function App() {
   return (
     <Container>
       <MainNavBar/>
+      <MainContainer>
+        
+      </MainContainer>
     </Container>
   );
 }
---
+++ front-end/src/components/MainContainer/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Container} from 'react-bootstrap'
+import './MainContainer.css'
+
+class MainContainer extends Component {
+    render() {
+        return (
+            <Container className="content-container">
+
+            </Container>
+        )
+    }
+}
+
+export default MainContainer
