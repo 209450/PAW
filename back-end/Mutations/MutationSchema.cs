9a2968acf6c1d1475626aa92ebdd36232ed20467
Wiktor <wiktor756@gmail.com>
2019-12-10 18:55:47 +0100

    tworzenie tokenu v1

--- back-end/Mutations/MutationSchema.cs
+++ back-end/Mutations/MutationSchema.cs
@@ -7,6 +7,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Task = Mutations.Tables.Task;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace back_end.Mutations
 {
@@ -186,7 +190,63 @@ namespace back_end.Mutations
                     return task;
                 }
             );
+            Field<UserType>(
+                "auth",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<UserInputType>> { Name = "data" }),
+                resolve: context =>
+                {
+                    var user = context.GetArgument<User>("data");
+                    Console.WriteLine(user.Name);
+
+                    var result = dbAdapter.Select(
+                        "SELECT * FROM trello.Users WHERE name = '" + user.Name + "' AND password = '" + user.Password + "'"
+                        );
+
+                    if (result == null) 
+                        return null;
+                    
+                    return CreateJWTAsync(user, "dupa", "Ni3-Wi3M_J4K_T3N-Kl00cZ_M4_VVyGl0ND4C-Pr053-MnI3-Z05T4VVIC", 7);
+                }
+            );
+            
+        }
+        public static async Task<string> CreateJWTAsync(
+            User user,
+            string issuer,
+            string symSec,
+            int daysValid)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var claims = await CreateClaimsIdentities(user);
+
+            JwtSecurityToken token = null;
+            try {
+            // Create JWToken
+            token = tokenHandler.CreateJwtSecurityToken(issuer: issuer,
+                audience: issuer,
+                subject: claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(daysValid),
+                signingCredentials:
+                new SigningCredentials(
+                    new SymmetricSecurityKey(
+                        Encoding.Default.GetBytes(symSec)),
+                        Microsoft.IdentityModel.Tokens.SecurityAlgorithms.HmacSha256Signature));
+            }catch(Exception e){
+                Console.Write(e.Message);
+            }
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        public static Task<ClaimsIdentity> CreateClaimsIdentities(User user)
+        {
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, user.Name));
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, "User"));
+
+            return System.Threading.Tasks.Task.FromResult(claimsIdentity);
         }
     }
-
 }
