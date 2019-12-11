0912af3427c373c3205be254c910f78a379eea37
Łukasz Malina <lukasz662@gmail.com>
2019-12-11 17:57:45 +0100

    Dodany odczyt usera z bazy danych

--- back-end/Services/UserService.cs
+++ back-end/Services/UserService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Mutations.Tables;
+using NReco.Data;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -20,30 +21,36 @@ namespace back_end.Services
     {
 
         private readonly AppSettings _appSettings;
+        private readonly DbDataAdapter _dbDataAdapter;
 
-        public UserService(IOptions<AppSettings> appSettings)
+        public UserService(IOptions<AppSettings> appSettings, DbDataAdapter dbDataAdapter)
         {
             _appSettings = appSettings.Value;
+            _dbDataAdapter = dbDataAdapter;
         }
 
         public User Authenticate(string username, string password)
         {
-            User user = new User();
-            user.Id = "1";
-            user.Name = "test";
-            user.Password = "dupa";
-            if (user == null)
+
+            var result = _dbDataAdapter.Select(
+                        "SELECT * FROM trello.Users WHERE name = '" + username + "' AND password = '" + password + "'"
+                        )
+                        .ToDictionary();
+
+            if (result == null)
                 return null;
-            if (user.Name != username || user.Password != password)
-                return null;
-            // authentication successful so generate jwt token
+            // uwierzytelnication successful so generate jwt token
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes("Ni3-Wi3M_J4K_T3N-Kl00cZ_M4_VVyGl0ND4C-Pr053-MnI3-Z05T4VVIC");//_appSettings.Secret);
+            User user = new User();
+            user.Id = result["id"].ToString();
+            user.Name = result["name"].ToString();
+            user.Password = result["password"].ToString();
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                    new Claim(ClaimTypes.Name, user.Id)
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
