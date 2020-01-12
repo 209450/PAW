a8ef19a4064d7fa7e8068e24b97f1dacdc6822da
Lucat0n <39059724+Lucat0n@users.noreply.github.com>
2020-01-12 16:43:08 +0100

    fix - nie znajduje konstruktora middleware'a, bo tego trzeba sobie wyczarować

--- back-end/AuthHelpers/GraphQLSetting.cs
+++ back-end/AuthHelpers/GraphQLSetting.cs
@@ -9,7 +9,7 @@ namespace back_end.AuthHelpers
 {
     class GraphQLSetting
     {
-        public Func<HttpContext, Task<object>> BuildUserContext { get; set; }
+        public Func<HttpContext, Task<GraphQLUserContext>> BuildUserContext { get; set; }
         public object Root { get; set; }
         public List<IValidationRule> ValidationRules { get; } = new List<IValidationRule>();
     }
